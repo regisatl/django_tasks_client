7d1e685b08fcf8f5d97539d732ea4571cdbea505
regisatl <regisattolou21@outlook.fr>
2024-02-05 15:41:24 +0100

    configuration de tailwind dans le projet

--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,7 @@ export default {
   },
 };
 </script>
-<style>
-#app {
-  font-family:  Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./assets/css/output";
+@import "./assets/css/tailwind";
 </style>
